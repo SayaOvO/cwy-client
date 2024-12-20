51e7f8adeb0d161029b8d02fe2efb284d697ccce
Saya <sayaOvO@pm.me>
2024-12-20 17:44:55 +0800

    forget to push one file

---
+++ src/types/langauge-types.ts
@@ -0,0 +1,11 @@
+export enum LanguageType {
+  JavaScript = 'javascript',
+  TypeScript = 'typescript',
+  JSX = 'jsx',
+  TSX = 'tsx',
+  CSS = 'css',
+  HTML = 'html',
+  JSON = 'json',
+  Markdown = 'markdown',
+  Plain = 'plain',
+}
